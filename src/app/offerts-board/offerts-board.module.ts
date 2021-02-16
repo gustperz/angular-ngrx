6f1b878a05c69471fa80c3abd82d2bd682c22463
Gustavo Pérez Machado <gustperz@gmail.com>
2021-02-15 19:36:09 -0500

    create components folder

--- src/app/offerts-board/offerts-board.module.ts
+++ src/app/offerts-board/offerts-board.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OfferComponent } from './offer/offer.component';
-import { CharacteristicsComponent } from './characteristics/characteristics.component';
-import { PricesComponent } from './prices/prices.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { CharacteristicsComponent } from './components/characteristics/characteristics.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
